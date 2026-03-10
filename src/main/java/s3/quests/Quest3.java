8c8b5c5a4c02dffd521f978f6f36e0fe79f18593
Brok3nPix3l <rjbabala@yahoo.com>
2026-03-09 22:33:04 -0400

    story 3 quest 3 part 1 wip

--- src/main/java/s3/quests/Quest3.java
+++ src/main/java/s3/quests/Quest3.java
@@ -5,61 +5,78 @@ import utils.StringUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Quest3 extends QuestLong {
-    private class Node {
-        private int id;
-        private String plug;
-        private String leftSocket;
-        private String rightSocket;
-        private Node left;
-        private Node right;
-        private Node parent;
-
-        public Node(int id, String plug, String leftSocket, String rightSocket) {
-            this.id = id;
-            this.plug = plug;
-            this.leftSocket = leftSocket;
-            this.rightSocket = rightSocket;
-        }
-    }
+    private Node cur;
+    private List<Node> nodes;
 
     @Override
     public long part1(String input) {
         long ans = 0L;
         List<String> lines = StringUtils.splitInput(input);
-        List<Node> nodes = new ArrayList<>();
-        for (String line : lines) {
-            String[] split = line.split(", ");
-            int id = Integer.parseInt(split[0]);
-            String plug = split[1];
-            String leftSocket = split[2];
-            String rightSocket = split[3];
-            nodes.add(new Node(id, plug, leftSocket, rightSocket));
+        nodes = lines.stream().map(Node::fromLine).collect(Collectors.toList());
+        Node head = nodes.removeFirst();
+        cur = nodes.removeFirst();
+        buildTree(head);
+        while (head.parent != null) {
+            head = head.parent;
         }
-        Node tree = nodes.removeFirst();
-        Node head = tree;
-        while (!lines.isEmpty()) {
-            Node cur = nodes.removeFirst();
-            if (Objects.equals(cur.plug, head.leftSocket)) {
+        ans = generateChecksum(head);
+        return ans;
+    }
+
+    private long generateChecksum(Node head) {
+        List<Integer> values = new ArrayList<>();
+        inorder(head, values);
+        long checksum = 0;
+        for (int i = 0; i < values.size(); i++) {
+            checksum += (long) values.get(i) * (i + 1);
+        }
+        return checksum;
+    }
+
+    private void inorder(Node head, List<Integer> values) {
+        if (head == null) {
+            return;
+        }
+        if (head.left != null) {
+            inorder(head.left, values);
+        }
+        values.add(head.id);
+        if (head.right != null) {
+            inorder(head.right, values);
+        }
+    }
+
+    private void buildTree(Node head) {
+        while (cur != null) {
+            if (head.left != null) {
+                buildTree(head.left);
+            } else if (Objects.equals(cur.plug, head.leftSocket)) {
                 head.left = cur;
                 cur.parent = head;
-                if (lines.isEmpty()) {
+                if (nodes.isEmpty()) {
+                    cur = null;
                     break;
                 }
                 cur = nodes.removeFirst();
             }
-            if (head.left != null) {
-
-            }
-            if (Objects.equals(cur.plug, head.rightSocket)) {
+            if (head.right != null) {
+                buildTree(head.right);
+            } else if (Objects.equals(cur.plug, head.rightSocket)) {
                 head.right = cur;
                 cur.parent = head;
+                if (nodes.isEmpty()) {
+                    cur = null;
+                    break;
+                }
+                cur = nodes.removeFirst();
+            }
+            if (head.parent != null) {
+                break;
             }
         }
-
-        ans = generateChecksum(tree);
-        return ans;
     }
 
     @Override
@@ -74,5 +91,36 @@ public class Quest3 extends QuestLong {
         long ans = 0L;
         List<String> lines = StringUtils.splitInput(input);
         return ans;
+    }
+
+    private static class Node {
+        private final int id;
+        private final String plug;
+        private final String leftSocket;
+        private final String rightSocket;
+        private Node left;
+        private Node right;
+        private Node parent;
+
+        public Node(int id, String plug, String leftSocket, String rightSocket) {
+            this.id = id;
+            this.plug = plug;
+            this.leftSocket = leftSocket;
+            this.rightSocket = rightSocket;
+        }
+
+        public static Node fromLine(String line) {
+            String[] split = line.split(", ");
+            int id = Integer.parseInt(split[0].split("=", 2)[1]);
+            String plug = split[1].split("=", 2)[1];
+            String leftSocket = split[2].split("=", 2)[1];
+            String rightSocket = split[3].split("=", 2)[1];
+            return new Node(id, plug, leftSocket, rightSocket);
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" + "id=" + id + '}';
+        }
     }
 }
